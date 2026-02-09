e8c4966d00b828ecd7acc37bf25ba20b08a3ae25
ropkyns <ropkyns@gmail.com>
2026-02-09 17:40:01 +0100

    d01 ex01

--- python_1/ex01/array2D.py
+++ python_1/ex01/array2D.py
@@ -1,1 +1,14 @@
 import numpy as np
+
+
+def slice_me(family: list, start: int, end: int) -> list:
+    assert isinstance(family, list), "1st arg is not a list"
+    assert np.ndim(family) == 2, "List is not 2D"
+    assert all(len(family[0]) is len(x) for x in family), "2nd dimensional list are not the same size"
+    try:
+        shape = np.shape(family)
+        print(f"My shape is : {shape}")
+        print(np.array(family))
+
+    except AssertionError as error:
+        print(f"AssertionError: {error}")
---
+++ python_1/ex02/load_image.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.image as mimg
+
+
+def ft_load(path: str) -> array:
+    
