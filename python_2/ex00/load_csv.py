4f027e826c8b9ae7cc6ef2b6f97646b6abab3d65
ropkyns <ropkyns@gmail.com>
2026-02-16 16:54:53 +0100

    day 02 ex00

--- python_2/ex00/load_csv.py
+++ python_2/ex00/load_csv.py
@@ -1,5 +1,14 @@
 import pandas as pd
 
 
-def load(path: str) -> Dataset:
-    #your code here
+def load(path: str) -> pd.DataFrame:
+    try:
+        assert path != "", "Path null"
+        assert path.endswith(".csv"), "File is not a .csv"
+        df = pd.read_csv(path)
+        assert df is not None, "File not found"
+        print(f"Loading dataset of dimensions {df.shape}")
+        return (df)
+    except Exception as error:
+        print(f"Error: {error}")
+        return None
